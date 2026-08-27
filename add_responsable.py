c1b29b8617b8dcc12aad85ccb0ca97cb487f7b35
javierazambranoch <javierazambranoch@users.noreply.github.com>
2026-08-27 08:59:50 -0400

    Remove one-off migration helper scripts

--- add_responsable.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host='aws-0-us-east-2.pooler.supabase.com', port='5432', dbname='postgres', user='postgres.noshouqodrmqqcegkddk', password='Daem2026Nacimiento')
-cur = conn.cursor()
-cur.execute("ALTER TABLE Computador ADD COLUMN IF NOT EXISTS responsable TEXT")
-conn.commit()
-print("Columna responsable agregada")
-conn.close()
--- check_computador.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host='aws-0-us-east-2.pooler.supabase.com', port='5432', dbname='postgres', user='postgres.noshouqodrmqqcegkddk', password='Daem2026Nacimiento')
-cur = conn.cursor()
-cur.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name='computador' ORDER BY ordinal_position")
-for r in cur.fetchall():
-    print(r)
-conn.close()
